00be310f871b47f9963614ff387523a82bfae937
zeynepkoyun <53746550+zeynepkoyun@users.noreply.github.com>
2021-01-09 22:35:21 +0300

    Update service.py

--- scoring-service/service.py
+++ scoring-service/service.py
@@ -29,7 +29,7 @@ from sklearn.base import BaseEstimator
 import os
 print(os.getcwd())
 print(os.listdir(os.getcwd()))
-print("digeri ise",os.listdir(os.path.join(os.getcwd(),"bodywork_project")))
+print("digeri ise",os.listdir(os.path.join(os.getcwd(),"main_page")))
 MODEL_PATH = 'main_page/scoring-service/classification_model.joblib'
 CLASS_TO_SPECIES_MAP = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
 
